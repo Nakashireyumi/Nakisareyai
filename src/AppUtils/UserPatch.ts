a824ec500d41da1c370138a632eb975b8b1305de
Cassitly <102461124+cassitly@users.noreply.github.com>
2025-11-17 08:00:23 +0700

    Update UserPatch.ts

--- src/AppUtils/UserPatch.ts
+++ src/AppUtils/UserPatch.ts
@@ -6,7 +6,7 @@ export default {
     "1056491867375673424": {
         id: "1056491867375673424",
         username: "revery.api",
-        global_name: "Revery",
+        global_name: "Revery", // used nakurity's naming scheme
         avatar: "93fb88f6b8c0a2a33c437d0fff4c6625",
         avatar_decoration: null,
         discriminator: "0000",
