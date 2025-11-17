0fe1ad0224e1f456e56f9e9b252de71dd1f1f31b
Cassitly <102461124+cassitly@users.noreply.github.com>
2025-11-17 07:59:45 +0700

    Update user information in UserPatch.ts

--- src/AppUtils/UserPatch.ts
+++ src/AppUtils/UserPatch.ts
@@ -5,8 +5,8 @@ import { APIUser } from "discord-api-types/v10";
 export default {
     "1056491867375673424": {
         id: "1056491867375673424",
-        username: "aiko-chan-ai",
-        global_name: "Elysia",
+        username: "revery.api",
+        global_name: "Revery",
         avatar: "93fb88f6b8c0a2a33c437d0fff4c6625",
         avatar_decoration: null,
         discriminator: "0000",
